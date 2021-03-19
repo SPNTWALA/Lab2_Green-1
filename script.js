acd2ef911a58d6297b3677cad6f1b0a981f2a50a
Станислав Дмитриевич Кирчанов <19300517@live.inueco.ru>
2021-03-19 11:37:32 +0500

    0.2

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
+import fs from 'fs'
+const string = fs.readFileSync(__dirname + '/deposits.json', 'utf8')
+let users = JSON.parse(string);
+//console.log ( x[1].Name )
+
+
+//console.log (Object.keys(x));
 
 function handler (){
     let name = document.getElementById("btn").value;
-    //alert (name); 
-    (name == "ФИО") ? alert ("Пользователь найден,но кода пока нема") : alert ("Данного клиента нет в базе данных");
+    for (let x=0;x<users.length;x++){
+      if (name == users[x].Name) { 
+      alert("У пользователя = "+users[x].Name+"\n"+"Начальный вклад = "+users[x].summ+"\n"+"Месяц вклада = "+users[x].month+"\n");
+      break
+      }
+    }
+    return name;
 }
 window.handler = handler;
-/*
-btn.onclick = handler;
-btn.onblur = handler;
-btn.onchange = handler;
-*/
+
+
+
+/*function search(){
+    let name = document.getElementById("btn").value;
+      for (let x=0;x<list.length;x++){
+        (list[x].Name == name) ? alert ("Ползователь найден"): alert("Ползователь не найден") ;
+      }
+}
+*/ 
