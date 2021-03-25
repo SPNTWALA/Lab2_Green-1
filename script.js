ca0df8043c95e654a5ecc230b5b36ef919890a26
Станислав Дмитриевич Кирчанов <19300517@live.inueco.ru>
2021-03-25 10:33:34 +0500

    0.9

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 import fs from 'fs'
 const string = fs.readFileSync(__dirname + '/deposits.json', 'utf8')
 let users = JSON.parse(string);
-//console.log ( x[1].Name )
 let user;
 
-//console.log (Object.keys(x));
 
 function handler (){
     let name = document.getElementById("btn").value;
@@ -17,8 +15,8 @@ function handler (){
       }
     }
     if (user != 0){
-      alert("У пользователя = "+user.Name+"\n"+"Начальный вклад = "+user.summ+"\n"+"Месяц вклада = "+user.month+"\n")
-      bank (user)
+      
+      bank(user);
       } 
     else {
       alert ("Пользователь не найден")
@@ -29,36 +27,46 @@ function handler (){
 window.handler = handler;
 
 function bank(user) {
+  let balancechange,summchange,percentchange;
+  let summ=Number(user.summ);
+  let percent=Number(user.percent);
+  let monthchanges=Number(user.monthchanges);
+  let length=Number(user.length);
+  let month=Number(user.month);
   percent=percent/12/100;
   percent=percent.toFixed(3);
-  for (let y=0; y<length; y++){
-    balancechange = Number(summ);
-    balancechange = Number(balancechange) + Number(balancechange)*Number(percent);
-    balancechange = Number(balancechange) + Number(monthchanges);
-    summchange = Number(balancechange) - Number(summ);
-    (balancechange>0) ? percentchange = Number(summ)/Number(balancechange) : percentchange = 1 - Number(balancechange) / Number(summ);
-    percentchange=percentchange.toFixed(2);
-    summchange=summchange.toFixed(2);
-    balancechange=balancechange.toFixed(2);
-    currentresult = {   
-      currentBalance: balancechange, 
-      balanceChange: summchange,
-      percentchange: percentchange * 100     
-    }
-    result.push (currentresult);
-    currentresult={};  
-    summ=balancechange;
-    if (summ < 0){
-      return console.log ('Снятие с счета превысило депозит')
-    }
-  }
-}
-
-
-/*function search(){
-    let name = document.getElementById("btn").value;
-      for (let x=0;x<list.length;x++){
-        (list[x].Name == name) ? alert ("Ползователь найден"): alert("Ползователь не найден") ;
+  if (month<length){
+      for (let y=0; y<month; y++){
+        balancechange = Number(summ);
+        balancechange = Number(balancechange) + Number(balancechange)*Number(percent);
+        balancechange = Number(balancechange) + Number(monthchanges);
+        summchange = Number(balancechange) - Number(summ);
+        (balancechange>0) ? percentchange = Number(summ)/Number(balancechange) : percentchange = 1 - Number(balancechange) / Number(summ);
+        percentchange=percentchange.toFixed(2);
+        summchange=summchange.toFixed(2);
+        balancechange=balancechange.toFixed(2);
+        summ=balancechange;
+        if (summ < 0){
+          return console.log ('Снятие с счета превысило депозит')
+        }
       }
+      alert("У пользователя = "+user.Name+"\n"+"Начальный вклад = "+user.summ+"\n"+"Срок вклада = "+user.length+"\n"+"Процент вклада = "+user.percent+"%"+"\n"+"Ежемесячное пополнение/снятие = "+user.monthchanges+"\n"+"Месяц вклада = "+user.month+"\n"+"Текущий баланс = "+balancechange)
+    } 
+    else {
+      for (let y=0; y<length; y++){
+        balancechange = Number(summ);
+        balancechange = Number(balancechange) + Number(balancechange)*Number(percent);
+        balancechange = Number(balancechange) + Number(monthchanges);
+        summchange = Number(balancechange) - Number(summ);
+        (balancechange>0) ? percentchange = Number(summ)/Number(balancechange) : percentchange = 1 - Number(balancechange) / Number(summ);
+        percentchange=percentchange.toFixed(2);
+        summchange=summchange.toFixed(2);
+        balancechange=balancechange.toFixed(2);
+        summ=balancechange;
+        if (summ < 0){
+          return console.log ('Снятие с счета превысило депозит')
+        }
+      }
+      alert ("Вклад закрыт"+"\n"+"Пользователь = "+user.Name+"\n"+"Начальный вклад = "+user.summ+"\n"+"Срок вклада = "+user.length+"\n"+"Процент вклада = "+user.percent+"%"+"\n"+"Ежемесячное пополнение/снятие = "+user.monthchanges+"\n"+"Итоговая сумма = "+balancechange);
+    }
 }
-*/ 
